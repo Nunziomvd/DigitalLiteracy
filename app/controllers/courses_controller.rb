754d81df177fe57d41949f91a1eb93db5ea9d76f
Student 1 Account <student1@hpc260.tcnj.edu>
2024-11-23 22:40:47 -0500

    TV course major complet

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,4 +1,32 @@
 class CoursesController < ApplicationController
-  def index
+  def phone
+  end
+
+  def tv
+  end
+
+  def tv_remote
+    render 'courses/tv_lessons/tv_remote'
+  end
+  def tv_homescreen
+    render 'courses/tv_lessons/tv_homescreen'
+  end
+  def tv_live
+    render 'courses/tv_lessons/tv_live'
+  end
+  def tv_maintain
+    render 'courses/tv_lessons/tv_maintain'
+  end
+  def tv_apps
+    render 'courses/tv_lessons/tv_apps'
+  end
+  def tv_voice
+    render 'courses/tv_lessons/tv_voice'
+  end
+  def tv_settings
+    render 'courses/tv_lessons/tv_settings'
+  end
+
+  def computer
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,27 @@
 Rails.application.routes.draw do
   # Define the root route to the home page
   root 'home#index'
-  
-  # Define a route for the Courses page
-  get 'courses', to: 'courses#index', as: 'courses'
 
-  # Optionally, you can remove the 'home/index' route since 'root' already points to it
-  # get 'home/index'
+  # Courses Routes
+  get 'courses', to: 'courses#index', as: 'courses'
+  get 'courses/phone', to: 'courses#phone', as: 'phone_course'
+  get 'courses/computer', to: 'courses#computer', as: 'computer_course'
+
+  # TV Course Routes
+  get 'courses/tv', to: 'courses#tv', as: 'tv_course'
+
+  # TV Lesson Routes (Grouped)
+  scope 'courses/tv', as: 'tv' do
+    get 'lessons/tv_remote', to: 'courses#tv_remote', as: 'remote'
+    get 'lessons/tv_homescreen', to: 'courses#tv_homescreen', as: 'homescreen'
+    get 'lessons/tv_apps', to: 'courses#tv_apps', as: 'apps'
+    get 'lessons/tv_live', to: 'courses#tv_live', as: 'live'
+    get 'lessons/tv_maintain', to: 'courses#tv_maintain', as: 'maintain'
+    get 'lessons/tv_voice', to: 'courses#tv_voice', as: 'voice'
+    get 'lessons/tv_settings', to: 'courses#tv_settings', as: 'settings'
+
+
+  end
 
   # Health check route
   get 'up' => 'rails/health#show', as: :rails_health_check
